55c2c55d828158cd2da452ad05cfaad155c66e72
ivantarabanovych <vanjokcitet334@gmail.com>
2024-09-23 23:30:19 +0300

    q2

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 const heading = document.createElement('h1');
-heading.textContent = 'This is first header!';
+heading.textContent = 'List element:';
 document.body.appendChild(heading);
 
 const button = document.createElement('button');
 button.classList.add('header-button');
-button.textContent = 'Change header';
+button.textContent = 'Add element';
 document.body.appendChild(button);
 
+const container = document.createElement('div');
+container.classList.add('container-elements');
+document.body.appendChild(container);
+
+
 button.addEventListener('click', () =>{
-    heading.textContent = 'Changed successfully!';
+    const newElement = document.createElement('div');
+    newElement.classList.add('new-element');
+
+    const elementCount = container.childElementCount + 1;
+    newElement.textContent = `Елемент ${elementCount}`;
+
+    container.appendChild(newElement);
 });
+
+
 
