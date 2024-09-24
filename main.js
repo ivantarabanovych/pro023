e401221c88b5929966c9e998f3b415c4f05b2dc7
ivantarabanovych <vanjokcitet334@gmail.com>
2024-09-24 10:16:34 +0300

    q4

--- main.js
+++ main.js
@@ -15,28 +15,34 @@ const filterInput = document.createElement("input");
 filterInput.setAttribute("placeholder", "Enter text for filter...");
 document.body.appendChild(filterInput);
 
-const taskList = document.createElement("li");
+const taskList = document.createElement("ul");
 document.body.appendChild(taskList);
 
-const addTask = () => {
-  const taskText = taskInput.value.trim();
-  if (taskText) {
-    const listItem = document.createElement("li");
-    listItem.textContent = taskText;
-    taskList.appendChild(listItem);
-    taskInput.value = "";
-    filterTasks();
-  }
+const deleteTask = (taskItem) => {
+    taskItem.remove(); 
 };
-const filterTasks = () => {
-  const filterText = filterInput.value.toLowerCase();
-  const items = taskList.getElementsByTagName("li");
 
-  for (let item of items) {
-    const text = item.textContent.toLowerCase();
-    item.style.display = text.includes(filterText) ? "" : "none";
-  }
-};
+const addTask = () =>{
+    const taskText = taskInput.value.trim();
+    if (taskText){
+        const listItem = document.createElement('li');
+
+        const taskContent = document.createElement('span');
+        taskContent.textContent = taskText;
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.classList.add('delete-btn');
+        deleteBtn.textContent = ('Delete');
+        deleteBtn.style.marginLeft = '10px';
+        deleteBtn.addEventListener('click', () => deleteTask(listItem));
+
+        listItem.appendChild(taskContent);
+        listItem.appendChild(deleteBtn);
+
+        taskList.appendChild(listItem);
+
+        filterInput.value = '';
+    }
+}
 
 taskButton.addEventListener('click', addTask);
-filterInput.addEventListener('input', filterTasks);
